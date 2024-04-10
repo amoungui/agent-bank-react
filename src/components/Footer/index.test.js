51b630fc4f408440fcaf1beb1bb6ce30ef8e582d
amoungui <samoungui@gmail.com>
2024-04-10 13:52:45 +0100

    write test for the Footer component

---
+++ src/components/Footer/index.test.js
@@ -0,0 +1,19 @@
+// Importation des fonctions 'render' et 'screen' depuis la bibliothèque '@testing-library/react'
+import { render, screen } from "@testing-library/react";
+
+// Importation du composant 'Hero' depuis le répertoire courant
+import Footer from "./index";
+
+// Début du bloc describe des tests pour le composant 'Footer'
+describe("When a Footer component is created", () => {
+    // Test spécifique pour vérifier que le copyrigth est affiché
+    it("Verify if copyright text is diplay",  () =>{
+        render(
+            <Footer />
+        )
+        const copyright = screen.getByText(/Copyright 2020 Argent Bank/);
+
+        // Vérification que les éléments sont présents dans le document
+        expect(copyright).toBeInTheDocument();    
+    });
+});
