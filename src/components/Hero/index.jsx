65942d628978b6856923e73de3044608b895a136
amoungui <samoungui@gmail.com>
2024-04-10 13:45:56 +0100

    write test for Hero component

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -6,7 +6,7 @@ function Hero() {
     };
 
     return (
-        <div style={heroStyle} className="hero">
+        <div style={heroStyle} className="hero" data-testid="hero-image-testid">
             <section class="hero-content">
             <h2 className="sr-only">Promoted Content</h2>
             <p className="subtitle">No fees.</p>
---
+++ src/components/Hero/index.test.js
@@ -0,0 +1,34 @@
+// Importation des fonctions 'render' et 'screen' depuis la bibliothèque '@testing-library/react'
+import { render, screen } from "@testing-library/react";
+
+// Importation du composant 'Hero' depuis le répertoire courant
+import Hero from "./index";
+
+import backgroundImage from '../../assets/img/bank-tree.jpeg';
+
+// Début du bloc describe des tests pour le composant 'Hero'
+describe("When a Hero component is created", () => {
+    // Test spécifique pour vérifier qu'une image est affichée avec une valeur alt
+    it("renders with background image", () => {
+        // Rendu de la carte de service avec une source d'image, une valeur alt et un enfant spécifiques
+        render(
+            <Hero />
+        );
+
+        const heroElement = screen.getByTestId('hero-image-testid');
+
+        expect(heroElement).toHaveStyle(`backgroundImage: url(${backgroundImage})`);
+    });
+
+    it("Verify if card text is diplay",  () =>{
+        render(
+            <Hero />
+        )
+        const subtitle1 = screen.getByText(/No fees./);
+        const subtitle2 =screen.getByText(/No minimum deposit./);
+
+        // Vérification que les éléments sont présents dans le document
+        expect(subtitle1).toBeInTheDocument();
+        expect(subtitle2).toBeInTheDocument();        
+    });
+});
