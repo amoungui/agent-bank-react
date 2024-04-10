f58d238abaee1ba85a9de2d731a72c34e8fd2e87
amoungui <samoungui@gmail.com>
2024-04-10 07:43:14 +0100

    integrate FeatureItem component to Feature component

--- src/components/Feature/index.jsx
+++ src/components/Feature/index.jsx
@@ -1,39 +1,38 @@
+import { FeatureItem } from "../FeatureItem";
+// eslint-disable-next-line import/order
+import { v4 } from 'uuid';
+
+const uuidv4 = v4;
+
 function Feature() {
 
     return (
         <section class="features">
             <h2 class="sr-only">Features</h2>
-            <div class="feature-item">
-                <img src="./img/icon-chat.png" alt="Chat Icon" class="feature-icon" />
-                <h3 class="feature-item-title">You are our #1 priority</h3>
-                <p>
-                    Need to talk to a representative? You can get in touch through our
-                    24/7 chat or through a phone call in less than 5 minutes.
-                </p>
-            </div>
-            <div class="feature-item">
-                <img
-                    src="./img/icon-money.png"
-                    alt="Chat Icon"
-                    class="feature-icon"
-                />
-                <h3 class="feature-item-title">More savings means higher rates</h3>
-                <p>
-                    The more you save with us, the higher your interest rate will be!
-                </p>
-            </div>
-            <div class="feature-item">
-                <img
-                    src="./img/icon-security.png"
-                    alt="Chat Icon"
-                    class="feature-icon"
-                />
-                <h3 class="feature-item-title">Security you can trust</h3>
-                <p>
-                    We use top of the line encryption to make sure your data and money
-                    is always safe.
-                </p>
-            </div>
+            <FeatureItem 
+                key={`Feature-item-1-${uuidv4()}`} 
+                imageSrc="./img/icon-chat.png"
+                imageAlt="Chat Icon" 
+                title="You are our #1 priority"
+                description="Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes."
+            >
+            </FeatureItem>
+            <FeatureItem 
+                key={`Feature-item-2-${uuidv4()}`} 
+                imageSrc="./img/icon-money.png"
+                imageAlt="Chat Icon" 
+                title="More savings means higher rates"
+                description="The more you save with us, the higher your interest rate will be!"
+            >
+            </FeatureItem>
+            <FeatureItem 
+                key={`Feature-item-3-${uuidv4()}`} 
+                imageSrc="./img/icon-security.png"
+                imageAlt="Chat Icon" 
+                title="Security you can trust"
+                description="We use top of the line encryption to make sure your data and money is always safe."
+            >
+            </FeatureItem>
       </section>
     )
 }
