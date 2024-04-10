992310d0bd41d7518f2deec6d0666700dd2754cf
amoungui <samoungui@gmail.com>
2024-04-10 07:54:25 +0100

    install react-router-dom and prop-types and review index.jsx of src folder

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './app/App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<Router>
+			<Header />
+			<Routes>
+				<Route path="/" element={<Home />} />
+			</Routes>
+			<Footer />
+		</Router>
+	</React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
