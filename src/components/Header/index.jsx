101a4191105eaf724d2c6ec409d5b535fb1321fa
amoungui <samoungui@gmail.com>
2024-04-10 14:25:41 +0100

    add test to verfy signin link of Header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -15,7 +15,7 @@ function Header() {
                     <h1 className="sr-only">Argent Bank</h1>
                 </Link>
                 <div>
-                    <Link className="main-nav-item" to="./sign-in.html">
+                    <Link className="main-nav-item" to="/">
                     <i className="fa fa-user-circle"></i>
                     Sign In
                     </Link>
--- src/components/Header/index.test.jsx
+++ src/components/Header/index.test.jsx
@@ -1,5 +1,5 @@
 // Importation des fonctions 'render' et 'screen' depuis la bibliothèque '@testing-library/react'
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent  } from "@testing-library/react";
 import { BrowserRouter as Router } from 'react-router-dom';
 
 // Importation du composant 'Header' depuis le répertoire courant
@@ -24,8 +24,23 @@ describe("When a Header component is created", () => {
         // Vérification que l'attribut alt de l'élément image est égal à "image-alt-text"
         expect(imageElement.alt).toEqual("Argent Bank Logo");        
     });
-    
+
+    // Test spécifique pour vérifier que le lien 'Sign In' est cliquable
     it("Check signin link", () => { 
-        // vérifier le lien Sign In au clik
+        render(
+            <Router>
+                <Header />
+            </Router>
+        )
+
+        // Récupération de l'élément lien par son texte
+        const linkElement = screen.getByText(/Sign In/i);
+
+        // Simulation d'un clic sur l'élément lien
+        fireEvent.click(linkElement);
+
+        // Ici, nous allons ajouter des vérifications en fonction de ce que vous attendez après le clic sur le lien
+        // si on attende une redirection vers une autre page, nous devons vérifier l'URL actuelle
+        expect(window.location.href).toBe('http://localhost/'); //pour le moment nous n'avons pas encore définir l'url de la page de connexion
     });
 });
