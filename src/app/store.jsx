a53f8ca72e377f5159262671adfbc7144200d2c4
amoungui <samoungui@gmail.com>
2024-04-12 10:51:38 +0100

    first implementation of store and add it to the index root project

---
+++ src/app/store.jsx
@@ -0,0 +1,27 @@
+import { configureStore } from "@reduxjs/toolkit"
+
+let state = {
+    value: null,
+    list: []
+};
+
+const reducer = (currentState, action ) => {
+    switch (action.type) {
+        case 'SIGN_IN':
+            const newData = [...currentState.list, action.payload]
+            return {...currentState, list: newData}
+        case 'SIGN_OUT':
+            const list = []
+            return {...currentState, list: list}
+
+        default:
+            return currentState
+    }
+}
+
+export const store = configureStore(
+    {
+        preloadedState: state,
+        reducer
+    }
+)
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import Home from './pages/Home/'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import SignIn from './pages/SignIn'
 import User from "./pages/User";
+import store from "./app/store"
 
 import './assets/css/main.css'
 
@@ -15,14 +17,16 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
 	<React.StrictMode>
-		<Router>
-			<Header />
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/signin" element={<SignIn />} />
-				<Route path="/user" element={<User />} />
-			</Routes>
-			<Footer />
-		</Router>
+		<Provider store={store}>
+			<Router>
+				<Header />
+				<Routes>				
+					<Route path="/" element={<Home />} />
+					<Route path="/signin" element={<SignIn />} />
+					<Route path="/user" element={<User />} />
+				</Routes>
+				<Footer />
+			</Router>		
+		</Provider>
 	</React.StrictMode>
 );
