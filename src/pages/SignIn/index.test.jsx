acabc3609ca322ba56d7cfff46d38d7e425ad0bd
amoungui <samoungui@gmail.com>
2024-05-01 16:32:32 +0100

    Utilisez waitFor pour attendre que l'appel réseau soit terminé

--- src/pages/SignIn/index.test.jsx
+++ src/pages/SignIn/index.test.jsx
@@ -1,4 +1,4 @@
-import { render, fireEvent, screen } from '@testing-library/react';
+import { render, fireEvent, screen, waitFor } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from '../../app/store';
@@ -6,7 +6,7 @@ import SignIn from './index';
 import nock from 'nock';
 
 describe('SignIn Component', () => {
-  test('should submit the form', () => {
+  test('should submit the form', async () => { // Notez l'utilisation de async ici
     nock('http://localhost:3001')
       .post('/api/v1/user/login')
       .reply(200, { body: { token: 'fake-token' } });
@@ -23,7 +23,7 @@ describe('SignIn Component', () => {
     fireEvent.input(screen.getByTestId('password'), { target: { value: 'password456' } });
     fireEvent.submit(screen.getByTestId('form'));
 
-    // Vérification que l'interception nock a été utilisée
-    expect(nock.isDone()).toBeTruthy();
+    // Utilisez waitFor pour attendre que l'appel réseau soit terminé
+    await waitFor(() => expect(nock.isDone()).toBeTruthy());
   });
 });
