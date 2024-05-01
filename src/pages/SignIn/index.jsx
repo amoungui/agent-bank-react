66cae88c81b313064cd2e9df12074c637c7a548b
amoungui <samoungui@gmail.com>
2024-05-01 16:28:48 +0100

    install nock librairie to mock api test

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,5 +1,5 @@
 import Field from "../../components/Field";
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { signIn } from '../../context/DataContext/index'
 
@@ -10,88 +10,77 @@ export const FIELD_TYPES = {
   PASSWORD: 4,
 };
 
-
 function SignIn() {
-	const navigate  = useNavigate();
-	const dispatch = useDispatch();
-	const auth = useSelector(state => state.auth); // access your auth state
-	console.log(auth);
+  const navigate  = useNavigate();
+  const dispatch = useDispatch();
 
-	const handleSignIn = async (event) => {
-		event.preventDefault();
-		const email = event.target.elements.username.value.toString();
-		const password = event.target.elements.password.value.toString();
-		if (email!== "" && password !== ""){
-			try {
-				const cache = await caches.open('user-login');
-				const cachedResponse = await cache.match("http://localhost:3001/api/v1/user/login");
-				let response;
-				if (cachedResponse) {
-					response = cachedResponse;
-				} else {
-					response = await fetch("http://localhost:3001/api/v1/user/login", {
-						method: "POST",
-						headers: { "Content-Type": "application/json" },
-						body: JSON.stringify({ email, password }),
-					});
-					if (!response.ok) {
-						const errorBody = await response.json();
-						console.error('Erreur lors de la connexion:', errorBody);
-						throw new Error(`HTTP error! status: ${response.status}`);
-					}
-					// Mettre en cache la réponse
-					cache.put("http://localhost:3001/api/v1/user/login", response.clone());
-				}
-				const data = await response.json();
-				const token = data.body.token;
-				dispatch(signIn(token));
-				navigate('/user'); // redirect to /user
-			} catch (error) {
-				console.error("Erreur lors de la connexion:", error);
-			}
-		}else{
-			console.log("username or password doesn't existe");
-		}		
-	};
+  const handleSignIn = async (event) => {
+    event.preventDefault();
+    const email = event.target.elements.username.value.toString();
+    const password = event.target.elements.password.value.toString();
+
+    if (email && password) {
+      try {
+        const response = await fetch("http://localhost:3001/api/v1/user/login", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ email, password }),
+        });
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        const token = data.body.token;
+        dispatch(signIn(token));
+        navigate('/user'); // redirect to /user
+      } catch (error) {
+        console.error("Erreur lors de la connexion:", error);
+      }
+    } else {
+      console.log("username or password doesn't exist");
+    }		
+  };
 		  
-	return (
-		<main className="main bg-dark">
-		  <section className="sign-in-content">
-			<i className="fa fa-user-circle sign-in-icon"></i>
-			<h1>Sign In</h1>
-			<form onSubmit={handleSignIn} data-testid="form">
-			  <div className="input-wrapper">
-				<Field
-				  type={FIELD_TYPES.INPUT_TEXT}
-				  placeholder=""
-				  label="Username"
-				  id="username"
-				  testId="username"
-				/>
-			  </div>
-			  <div className="input-wrapper">
-				<Field
-				  type={FIELD_TYPES.PASSWORD}
-				  placeholder=""
-				  label="Password"
-				  id="password"
-				  testId="password"
-				/>
-			  </div>
-			  <div className="input-remember">
-				<Field
-				  type={FIELD_TYPES.CHECKBOX}
-				  placeholder=""
-				  label="Remember me"
-				  id="remember-me"
-				  testId="remember-me"
-				/>
-			  </div>
-			  <input type="submit" value="Sign In" className="sign-in-button" />
-			</form>
-		  </section>
-		</main>
-	);
+  return (
+    <main className="main bg-dark">
+      <section className="sign-in-content">
+        <i className="fa fa-user-circle sign-in-icon"></i>
+        <h1>Sign In</h1>
+        <form onSubmit={handleSignIn} data-testid="form">
+          <div className="input-wrapper">
+            <Field
+              type={FIELD_TYPES.INPUT_TEXT}
+              placeholder=""
+              label="Username"
+              id="username"
+              testId="username"
+            />
+          </div>
+          <div className="input-wrapper">
+            <Field
+              type={FIELD_TYPES.PASSWORD}
+              placeholder=""
+              label="Password"
+              id="password"
+              testId="password"
+            />
+          </div>
+          <div className="input-remember">
+            <Field
+              type={FIELD_TYPES.CHECKBOX}
+              placeholder=""
+              label="Remember me"
+              id="remember-me"
+              testId="remember-me"
+            />
+          </div>
+          <input type="submit" value="Sign In" className="sign-in-button" />
+        </form>
+      </section>
+    </main>
+  );
 }
 
 export default SignIn;
--- src/pages/SignIn/index.test.jsx
+++ src/pages/SignIn/index.test.jsx
@@ -3,22 +3,13 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from '../../app/store';
 import SignIn from './index';
-
-// Importation de jest-fetch-mock
-import fetchMock from 'jest-fetch-mock';
-
-// Utilisation de jest-fetch-mock à la place de fetch
-global.fetch = fetchMock;
+import nock from 'nock';
 
 describe('SignIn Component', () => {
-  beforeEach(() => {
-    // Réinitialisation des moqueries avant chaque test
-    fetchMock.resetMocks();
-  });
-
   test('should submit the form', () => {
-    // Mise en place de la moquerie pour l'appel de connexion
-    fetchMock.mockResponseOnce(JSON.stringify({ body: { token: 'fake-token' } }));
+    nock('http://localhost:3001')
+      .post('/api/v1/user/login')
+      .reply(200, { body: { token: 'fake-token' } });
 
     render(
       <Provider store={store}>
@@ -28,16 +19,11 @@ describe('SignIn Component', () => {
       </Provider>
     );
 
-    fireEvent.input(screen.getByTestId('username'), { target: { value: 'test@test.com' } });
-    fireEvent.input(screen.getByTestId('password'), { target: { value: 'password' } });
+    fireEvent.input(screen.getByTestId('username'), { target: { value: 'steve@rogers.com' } });
+    fireEvent.input(screen.getByTestId('password'), { target: { value: 'password456' } });
     fireEvent.submit(screen.getByTestId('form'));
 
-    // Vérification que fetch a été appelé avec les bonnes données
-    expect(fetchMock.mock.calls[0][0]).toEqual('http://localhost:3001/api/v1/user/login');
-    expect(fetchMock.mock.calls[0][1]).toEqual({
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email: 'test@test.com', password: 'password' }),
-    });
+    // Vérification que l'interception nock a été utilisée
+    expect(nock.isDone()).toBeTruthy();
   });
 });
