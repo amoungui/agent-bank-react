f66ae64efe960aeae0b0991cc68a0117f6da046d
amoungui <samoungui@gmail.com>
2024-05-02 08:04:08 +0100

    review className of error component

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,11 +1,14 @@
 import { Link } from 'react-router-dom'
+// import { useSelector } from 'react-redux';
 
 function Error() {
     return (
-        <main className="noexist-page">
-            <span className="noexist-page-info">404</span>
-            <p className="noexist-page-msg">Oups! La page que vous demandez n'existe pas.</p>
-            <Link to="/" className="noexist-page-link">Retourner sur la page d’accueil</Link>
+        <main className="main bg-dark">
+            <section className="sign-in-content">
+                <span className="noexist-page-info">404</span>
+                <p className="noexist-page-msg">Oups! La page que vous demandez n'existe pas.</p>
+                <Link to="/" className="noexist-page-link">Retourner sur la page d’accueil</Link>
+            </section>
         </main>
     )
 }
