72fb9365ad9aa004b6aaa4d3dcbc3f9be71a025d
amoungui <samoungui@gmail.com>
2024-05-02 17:38:25 +0100

    comment Hero component

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,11 +1,16 @@
+// Importation de l'image de fond depuis le fichier '../../assets/img/bank-tree.webp'
 import backgroundImage from '../../assets/img/bank-tree.webp';
 
+// Définition du composant Hero
 function Hero() {
+    // Définition du style du héros, qui inclut l'image de fond
     const heroStyle = {
         backgroundImage: `url(${backgroundImage})`,
     };
 
+    // Rendu du composant Hero
     return (
+        // Début du div du héros avec le style défini et une classe CSS
         <div style={heroStyle} className="hero" data-testid="hero-image-testid">
             <section className="hero-content">
             <h2 className="sr-only">Promoted Content</h2>
@@ -14,8 +19,9 @@ function Hero() {
             <p className="subtitle">High interest rates.</p>
             <p className="text">Open a savings account with Argent Bank today!</p>
             </section>
-        </div>
+        </div> // Fin du div du héros
     )
 }
 
-export default Hero
+// Exportation du composant Hero pour être utilisé dans d'autres parties de l'application
+export default Hero;
