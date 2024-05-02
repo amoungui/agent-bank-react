8b7ac6cb32c7585733f15abef9abad54865b9803
amoungui <samoungui@gmail.com>
2024-05-02 18:13:01 +0100

    comment SignIn component

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,8 +1,13 @@
+// Importation du composant Field
 import Field from "../../components/Field";
+// Importation du hook useDispatch de Redux
 import { useDispatch } from 'react-redux';
+// Importation du hook useNavigate de react-router-dom
 import { useNavigate } from 'react-router-dom';
+// Importation de la fonction signIn
 import { signIn } from '../../context/DataContext/index'
 
+// Définition des types de champs
 export const FIELD_TYPES = {
   INPUT_TEXT: 1,
   TEXTAREA: 2,
@@ -10,40 +15,58 @@ export const FIELD_TYPES = {
   PASSWORD: 4,
 };
 
+// Définition de la fonction SignIn
 function SignIn() {
+  // Utilisation du hook useNavigate
   const navigate  = useNavigate();
+  // Utilisation du hook useDispatch
   const dispatch = useDispatch();
 
+  // Définition de la fonction handleSignIn
   const handleSignIn = async (event) => {
+    // Prévention du comportement par défaut de l'événement
     event.preventDefault();
+    // Récupération de l'email et du mot de passe à partir des éléments de l'événement
     const email = event.target.elements.username.value.toString();
     const password = event.target.elements.password.value.toString();
 
+    // Si l'email et le mot de passe existent
     if (email && password) {
+      // Bloc try pour gérer les erreurs
       try {
+        // Effectuer une requête fetch pour se connecter
         const response = await fetch("http://localhost:3001/api/v1/user/login", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ email, password }),
         });
 
+        // Si la réponse n'est pas ok, lever une erreur
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
 
+        // Conversion de la réponse en JSON
         const data = await response.json();
+        // Récupération du token
         const token = data.body.token;
+        // Dispatch de la fonction signIn avec le token
         dispatch(signIn(token));
-        navigate('/user'); // redirect to /user
+        // Redirection vers '/user'
+        navigate('/user');
       } catch (error) {
+        // Affichage de l'erreur dans la console
         console.error("Erreur lors de la connexion:", error);
       }
     } else {
+      // Affichage d'un message d'erreur si l'email ou le mot de passe n'existent pas
       console.log("username or password doesn't exist");
     }		
   };
 		  
+  // Rendu du composant
   return (
+    // Balise principale
     <main className="main bg-dark">
       <section className="sign-in-content">
         <i className="fa fa-user-circle sign-in-icon"></i>
@@ -83,4 +106,5 @@ function SignIn() {
   );
 }
 
+// Exportation par défaut de la fonction SignIn
 export default SignIn;
--- src/pages/SignIn/index.test.jsx
+++ src/pages/SignIn/index.test.jsx
@@ -1,16 +1,26 @@
+// Importation des fonctions nécessaires de la bibliothèque de test
 import { render, fireEvent, screen, waitFor } from '@testing-library/react';
+// Importation du composant Router de react-router-dom
 import { BrowserRouter as Router } from 'react-router-dom';
+// Importation du composant Provider de react-redux
 import { Provider } from 'react-redux';
+// Importation du store Redux
 import store from '../../app/store';
+// Importation du composant SignIn
 import SignIn from './index';
+// Importation de la bibliothèque nock pour simuler des requêtes HTTP
 import nock from 'nock';
 
+// Début de la suite de tests pour le composant SignIn
 describe('SignIn Component', () => {
+  // Test pour vérifier la soumission du formulaire
   test('should submit the form', async () => { // Notez l'utilisation de async ici
+    // Configuration de nock pour intercepter une requête POST vers '/api/v1/user/login'
     nock('http://localhost:3001')
       .post('/api/v1/user/login')
       .reply(200, { body: { token: 'fake-token' } });
 
+    // Rendu du composant SignIn à tester, enveloppé dans les composants Provider et Router
     render(
       <Provider store={store}>
         <Router>
@@ -19,11 +29,14 @@ describe('SignIn Component', () => {
       </Provider>
     );
 
+    // Simulation de l'entrée de l'utilisateur dans le champ 'username'
     fireEvent.input(screen.getByTestId('username'), { target: { value: 'steve@rogers.com' } });
+    // Simulation de l'entrée de l'utilisateur dans le champ 'password'
     fireEvent.input(screen.getByTestId('password'), { target: { value: 'password456' } });
+    // Simulation de la soumission du formulaire
     fireEvent.submit(screen.getByTestId('form'));
 
-    // Utilisez waitFor pour attendre que l'appel réseau soit terminé
+    // Utilisation de waitFor pour attendre que l'appel réseau soit terminé
     await waitFor(() => expect(nock.isDone()).toBeTruthy());
   });
 });
