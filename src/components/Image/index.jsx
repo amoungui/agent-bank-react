93bc28791560fd5c25eeddb5fcf3bac02f8d1781
amoungui <samoungui@gmail.com>
2024-05-02 17:42:02 +0100

    comment Image component

--- src/components/Image/index.jsx
+++ src/components/Image/index.jsx
@@ -1,26 +1,33 @@
+// Importation de PropTypes pour la validation des props
 import PropTypes from "prop-types";
 
+// Définition du composant Image
 function Image({ imageSrc, imageAlt, dataTestid }) {
 
-	return (
+    // Rendu du composant Image
+    return (
+        // Début de l'élément img avec les attributs src, alt, className et data-testid
         <img
-            data-testid= {dataTestid}
-            src={process.env.PUBLIC_URL + imageSrc}
-            alt={imageAlt}
-            className="feature-icon"
-        />
-	)
+            data-testid= {dataTestid} // Attribut data-testid pour les tests
+            src={process.env.PUBLIC_URL + imageSrc} // Source de l'image
+            alt={imageAlt} // Texte alternatif de l'image
+            className="feature-icon" // Classe CSS de l'image
+        /> // Fin de l'élément img
+    )
 }
 
+// Validation des props du composant Image
 Image.propTypes = {
-	imageSrc: PropTypes.string.isRequired,
-	imageAlt: PropTypes.string,
-    dataTestid: PropTypes.string,
+    imageSrc: PropTypes.string.isRequired, // La source de l'image est requise et doit être une chaîne de caractères
+    imageAlt: PropTypes.string, // Le texte alternatif de l'image doit être une chaîne de caractères
+    dataTestid: PropTypes.string, // L'ID de test doit être une chaîne de caractères
 };
 
+// Définition des props par défaut du composant Image
 Image.defaultProps = {
-	imageAlt: "image",
-    dataTestid: "feature-item-image-testid"
+    imageAlt: "image", // Le texte alternatif par défaut est "image"
+    dataTestid: "feature-item-image-testid" // L'ID de test par défaut est "feature-item-image-testid"
 }
 
-export default Image
+// Exportation du composant Image pour être utilisé dans d'autres parties de l'application
+export default Image;
