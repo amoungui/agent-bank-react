16d0d2e349a755e529041dc0c8e81b5c3408eea3
amoungui <samoungui@gmail.com>
2024-05-02 18:19:50 +0100

    comment index code of react app

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,12 @@
+// Importation de React
 import React from 'react'
+// Importation de ReactDOM
 import ReactDOM from "react-dom/client";
+// Importation des composants nécessaires de react-router-dom
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+// Importation du composant Provider de react-redux
 import { Provider } from 'react-redux'
+// Importation des composants de l'application
 import Home from './pages/Home/'
 import Header from './components/Header'
 import Footer from './components/Footer'
@@ -9,21 +14,23 @@ import Error from './components/Error'
 import SignIn from './pages/SignIn'
 import User from "./pages/user";
 import Profil from './pages/Profil';
+// Importation du store Redux
 import store from "./app/store"
 
+// Importation du CSS principal
 import './assets/css/main.min.css'
 
-
-
-
+// Création d'une racine pour le rendu de l'application
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+// Rendu de l'application
 root.render(
+	// Utilisation du mode strict de React
 	<React.StrictMode>
 		<Provider store={store}>
 			<Router>
 				<Header />
-				<Routes>				
+				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="/signin" element={<SignIn />} />
 					<Route path="/user" element={<User />} />
