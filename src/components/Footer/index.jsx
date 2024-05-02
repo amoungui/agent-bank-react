b9aadc2f890fea7a8529eb523403b85ec36c50e5
amoungui <samoungui@gmail.com>
2024-05-02 17:23:50 +0100

    comment Footer component code

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,14 @@
+// Définition du composant Footer
 function Footer() {
 
+    // Rendu du composant Footer
     return (
+        // Début du pied de page
         <footer className="footer">
             <p className="footer-text">Copyright 2020 Argent Bank</p>
-        </footer>
+        </footer> // Fin du pied de page
     )
 }
 
-export default Footer
+// Exportation du composant Footer pour être utilisé dans d'autres parties de l'application
+export default Footer;
