49a0b7eca82d01964368d12095841a3a0457f78f
amoungui <samoungui@gmail.com>
2024-05-02 16:49:13 +0100

    add comments to Error component

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,8 +1,11 @@
+// Importation du composant Link depuis la bibliothèque react-router-dom
 import { Link } from 'react-router-dom'
-// import { useSelector } from 'react-redux';
 
+// Définition du composant Error
 function Error() {
+    // Rendu du composant Error
     return (
+        // Début du contenu principal
         <main className="main bg-dark">
             <section className="sign-in-content">
                 <span className="noexist-page-info">404</span>
@@ -10,7 +13,8 @@ function Error() {
                 <Link to="/" className="noexist-page-link">Retourner sur la page d’accueil</Link>
             </section>
         </main>
-    )
+    ) // Fin du contenu principal
 }
  
-export default Error
+// Exportation du composant Error pour être utilisé dans d'autres parties de l'application
+export default Error;
